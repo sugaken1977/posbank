1e12a03da96a8ebd485cf40f5d0f92d0c7f50c2f
vietvu-nguyen <vietvu.hcm@gmail.com>
2018-09-27 15:49:49 +0900

    Added asyncvalidate fetch

--- src/components/Signup/asyncValidate.js
+++ src/components/Signup/asyncValidate.js
@@ -2,7 +2,16 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const asyncValidate = (values /*, dispatch */) => {
   return sleep(1000).then(() => { // simulate server latency
-    if (['john', 'paul', 'george', 'ringo'].includes(values.username)) {
+  	fetch('http://localhost:5001/asyncvalidate',{
+  		method: 'POST',
+  		headers: {'Content-Type': 'application/json'},
+	        body: JSON.stringify({
+	          email: values.email
+	        })	
+  	})
+  	.then(response => response.json())
+  	.then(existed => console.log(existed))
+    if (['john', 'paul', 'george', 'ringo'].includes(values.email)) {
     	var error = {username: 'That username is taken'}
     	throw error;
     }
